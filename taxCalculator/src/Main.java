ba67b7cba0711313958b0029103b0112183095fe
emiralpulger <emiralprc@gmail.com>
2023-05-25 20:18:49 +0300

    Project Changed

--- taxCalculator/src/Main.java
+++ taxCalculator/src/Main.java
@@ -4,23 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         // creating variables
-        double amount, tax, taxAmount,taxedPrice;
+        double amount, taxAmount,taxedPrice = 0;
 
         // define Scanner class
         Scanner input = new Scanner(System.in);
 
         // get value from user
         System.out.print("Enter amount: ");
-        amount = input.nextInt();
-        System.out.print("Enter tax: ");
-        tax = input.nextInt();
-
-        // tax processing
-        taxAmount = (amount * tax) /100;
-        taxedPrice = taxAmount + amount;
+        amount = input.nextDouble();
+        
+        if (amount > 0 && amount <= 1000){
+            taxAmount = (amount * 18) /100;
+            taxedPrice = taxAmount + amount;
+        } else if (amount > 1000) {
+            taxAmount = (amount * 8) /100;
+            taxedPrice = taxAmount + amount;
+        }
 
         // information area
-        System.out.println("Taxed amount: " + tax);
         System.out.println("Tax free price: " + amount);
         System.out.println("Taxed price: " + taxedPrice);
     }
